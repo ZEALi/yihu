e0e601e42bd56f3aa88b0c67ed7796aea88bbc6a
ZEAL <zealli@gmail.com>
2013-12-05 10:15:11 +0800

    Update demo.js

--- standard/demo/demo.js
+++ standard/demo/demo.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
+(function(){
   $.popupTips("hello world");
-});
+  $('.btn-operate-blue').on('click',function(e){
+    e.preventDefault();
+    $.openDlg('/echo/html','hello world');
+  });
+})();
