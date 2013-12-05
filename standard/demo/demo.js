dfff4ba9730cb6504eb0bc5377f41e4136c342a9
ZEAL <zealli@gmail.com>
2013-12-05 11:07:09 +0800

    Update demo.js

--- standard/demo/demo.js
+++ standard/demo/demo.js
@@ -1,4 +1,9 @@
-$(window).off('resize').on('domready',function(e){
+try{$.bindLiveDetailViewHandler();}catch(e){}
+try{$.bindLiveContentSwitcherHandler();}catch(e){}
+try{$.bindLiveAutoCompleteHandler();}catch(e){}
+try{$.bindLiveContentPopupHandler();}catch(e){}
+try{$.bindLiveCancelPopupHandler();}catch(e){}
+$(window).on('load',function(e){
   $.popupTips("hello world",8,'center');
   $('.btn-operate-blue').on('click',function(e){
     e.preventDefault();
