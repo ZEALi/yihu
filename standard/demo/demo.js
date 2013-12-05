ae49b3357f5363f862bd9079a5e0eda796fab8fb
ZEAL <zealli@gmail.com>
2013-12-05 10:54:55 +0800

    Update demo.js

--- standard/demo/demo.js
+++ standard/demo/demo.js
@@ -1,7 +1,7 @@
-(function(){
-  $.popupTips("hello world");
+$(window).off('resize').on('domready',function(e){
+  $.popupTips("hello world",8,'center');
   $('.btn-operate-blue').on('click',function(e){
     e.preventDefault();
     $.openDlg('/gh/get/response.html/ZEALi/yihu/tree/master/standard/demo/','hello world');
   });
-})();
+});
